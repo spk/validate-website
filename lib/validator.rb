f6a82418abed40e65bfdcccc6a726dbbba767a79
Laurent Arnoud <laurent@spkdev.net>
2010-09-14 00:47:31 +0200

    lib/validator.rb: cleanup and rescue on Nokogiri::XML::SyntaxError
    Signed-off-by: Laurent Arnoud <laurent@spkdev.net>

--- lib/validator.rb
+++ lib/validator.rb
@@ -1,34 +1,35 @@
 class Validator
   XHTML_PATH = File.join(File.dirname(__FILE__), '..', 'lib', 'xhtml')
 
-  attr_accessor :page
-  attr_accessor :dtd
-  attr_accessor :namespace
-  attr_accessor :xsd
-  attr_accessor :errors
+  attr_reader :page, :dtd, :doc, :namespace, :xsd, :errors
 
   def initialize(page)
     @page = page
-    if @page.doc.internal_subset.system_id
-      @dtd = URI.parse(@page.doc.internal_subset.system_id)
+    @dtd = @page.doc.internal_subset
+
+    if @dtd.system_id
+      @dtd_uri = URI.parse(@dtd.system_id)
       # http://www.w3.org/TR/xhtml1/#dtds
-      @namespace = File.basename(@dtd.path, '.dtd')
-      fixed_dtd = @page.body.sub(@dtd.to_s, @namespace + '.dtd')
-      doc = Dir.chdir(XHTML_PATH) do
+      @namespace = File.basename(@dtd_uri.path, '.dtd')
+      fixed_dtd = @page.body.sub(@dtd_uri.to_s, @namespace + '.dtd')
+      @doc = Dir.chdir(XHTML_PATH) do
         Nokogiri::XML(fixed_dtd) { |cfg|
           cfg.noent.dtdload.dtdvalid
         }
       end
+
       # http://www.w3.org/TR/xhtml1-schema/
       @xsd = Dir.chdir(XHTML_PATH) do
         if File.exists?(@namespace + '.xsd')
           Nokogiri::XML::Schema(File.read(@namespace + '.xsd'))
         end
       end
-      @errors = @xsd ? @xsd.validate(doc) : ["Don't have this xsd"]
+      @errors = @xsd ? @xsd.validate(@doc) : ["Don't have this xsd"]
     else
-      @errors = ['No DTD set !']
+      @errors = ['DTD unknown']
     end
+  rescue Nokogiri::XML::SyntaxError => e
+    @errors = [e]
   end
 
   def valid?
