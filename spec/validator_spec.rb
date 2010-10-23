cbcc436fa9a0e23503858f368bc5dde054e8c4f2
Laurent Arnoud <laurent@spkdev.net>
2010-10-23 16:48:14 +0200

    Add Validator spec file, rename and add html test on validate_website_spec
    Signed-off-by: Laurent Arnoud <laurent@spkdev.net>

---
+++ spec/validator_spec.rb
@@ -0,0 +1,34 @@
+require File.dirname(__FILE__) + '/spec_helper'
+
+describe Validator do
+    before(:each) do
+      FakeWeb.clean_registry
+      @http = Anemone::HTTP.new
+    end
+
+    it "xhtml1-strict should be valid" do
+      name = 'xhtml1-strict'
+      dtd_uri = 'http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd'
+      file = File.join('spec', 'data', "#{name}.html")
+      page = FakePage.new(name,
+                          :body => open(file).read,
+                          :content_type => 'text/html')
+      @xhtml1_page = @http.fetch_page(page.url)
+      validator = Validator.new(@xhtml1_page)
+      validator.dtd.system_id.should == dtd_uri
+      validator.namespace.should == name
+      validator.should be_valid
+    end
+
+    it "html5 should be valid" do
+      pending("need to work on html5")
+      name = 'html5'
+      file = File.join('spec', 'data', "#{name}.html")
+      page = FakePage.new(name,
+                          :body => open(file).read,
+                          :content_type => 'text/html')
+      @html5_page = @http.fetch_page(page.url)
+      validator = Validator.new(@html5_page)
+      validator.should be_valid
+    end
+end
