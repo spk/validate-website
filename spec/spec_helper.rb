250638dcfcaf5dd72e6f10a069d830185b5efebc
Laurent Arnoud <laurent@spkdev.net>
2010-11-02 21:55:17 +0100

    Better requirement and remove require 'rubygems' from spec/spec_helper.rb
    lunch test:
     * rake spec
     * rspec spec/*_spec.rb
     * ruby{1.8,1.9.1,1.9.2} -S rspec --backtrace spec/*_spec.rb
    
    Signed-off-by: Laurent Arnoud <laurent@spkdev.net>

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,9 @@
-require 'rubygems'
-require File.dirname(__FILE__) + '/fakeweb_helper'
-
-$:.unshift(File.dirname(__FILE__) + '/../lib/')
+require File.expand_path(File.join(File.dirname(__FILE__), 'fakeweb_helper'))
 require 'anemone'
+
+lib_dir = File.join(File.dirname(__FILE__), '..', 'lib')
+$:.unshift(File.expand_path(lib_dir))
+
 require 'validate_website'
 
 SPEC_DOMAIN = 'http://www.example.com/'
