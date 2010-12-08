6afa3cffebfca0938b46db7184db95d202bbd2a7
Laurent Arnoud <laurent@spkdev.net>
2010-12-09 00:01:25 +0100

    Add linuxfr html5 page (should be valid)
    but the html5lib ruby is not up-to-date with the last spec
    
    Signed-off-by: Laurent Arnoud <laurent@spkdev.net>

--- spec/validator_spec.rb
+++ spec/validator_spec.rb
@@ -6,39 +6,60 @@ describe Validator do
       @http = Anemone::HTTP.new
     end
 
-    it "xhtml1-strict should be valid" do
-      name = 'xhtml1-strict'
-      dtd_uri = 'http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd'
-      file = File.join('spec', 'data', "#{name}.html")
-      page = FakePage.new(name,
-                          :body => open(file).read,
-                          :content_type => 'text/html')
-      @xhtml1_page = @http.fetch_page(page.url)
-      validator = Validator.new(@xhtml1_page)
-      validator.dtd.system_id.should == dtd_uri
-      validator.namespace.should == name
-      validator.should be_valid
+    describe("xhtml1") do
+      it "xhtml1-strict should be valid" do
+        name = 'xhtml1-strict'
+        dtd_uri = 'http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd'
+        file = File.join('spec', 'data', "#{name}.html")
+        page = FakePage.new(name,
+                            :body => open(file).read,
+                            :content_type => 'text/html')
+        @xhtml1_page = @http.fetch_page(page.url)
+        validator = Validator.new(@xhtml1_page)
+        validator.dtd.system_id.should == dtd_uri
+        validator.namespace.should == name
+        validator.should be_valid
+      end
     end
 
-    it "html5 should be valid" do
-      name = 'html5'
-      file = File.join('spec', 'data', "#{name}.html")
-      page = FakePage.new(name,
-                          :body => open(file).read,
-                          :content_type => 'text/html')
-      @html5_page = @http.fetch_page(page.url)
-      validator = Validator.new(@html5_page)
-      validator.should be_valid
+    describe('html5') do
+      context('when valid') do
+        it "html5 should be valid" do
+          name = 'html5'
+          file = File.join('spec', 'data', "#{name}.html")
+          page = FakePage.new(name,
+                              :body => open(file).read,
+                              :content_type => 'text/html')
+          @html5_page = @http.fetch_page(page.url)
+          validator = Validator.new(@html5_page)
+          validator.should be_valid
+        end
+      end
+      context('should be valid') do
+        it "with DLFP" do
+          pending("update html5lib ruby ?")
+          name = 'html5'
+          file = File.join('spec', 'data', "#{name}-linuxfr.html")
+          page = FakePage.new(name,
+                              :body => open(file).read,
+                              :content_type => 'text/html')
+          @html5_page = @http.fetch_page(page.url)
+          validator = Validator.new(@html5_page)
+          validator.should be_valid
+        end
+      end
     end
 
-    it 'should validate html4' do
-      name = 'html4-strict'
-      file = File.join('spec', 'data', "#{name}.html")
-      page = FakePage.new(name,
-                          :body => open(file).read,
-                          :content_type => 'text/html')
-      @html4_strict_page = @http.fetch_page(page.url)
-      validator = Validator.new(@html4_strict_page)
-      validator.should be_valid
+    describe('html4') do
+      it 'should validate html4' do
+        name = 'html4-strict'
+        file = File.join('spec', 'data', "#{name}.html")
+        page = FakePage.new(name,
+                            :body => open(file).read,
+                            :content_type => 'text/html')
+        @html4_strict_page = @http.fetch_page(page.url)
+        validator = Validator.new(@html4_strict_page)
+        validator.should be_valid
+      end
     end
 end
