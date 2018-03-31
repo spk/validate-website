c8752ef5affb1172133262cb81c79f2d4f00b8fd
Laurent Arnoud <laurent@spkdev.net>
2018-03-31 14:54:16 +0200

    Move coveralls before spec helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,10 +5,10 @@ require 'spidr'
 
 require 'validate_website/core'
 
-require File.expand_path('../webmock_helper', __FILE__)
-
 require 'coveralls'
 Coveralls.wear!
+
+require File.expand_path('../webmock_helper', __FILE__)
 
 TEST_DOMAIN = 'http://www.example.com/'.freeze
 ENV['LC_ALL'] = 'C.UTF-8' if defined?(RUBY_ENGINE) && RUBY_ENGINE == 'jruby'
